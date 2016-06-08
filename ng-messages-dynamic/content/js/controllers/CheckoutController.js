747ec6c1c67a9a87b1c1441fa669b480d79935e8
Todd Motto <todd@toddmotto.com>
2016-06-08 19:22:44 +0100

    chore: Push final ngMessage modules

---
+++ ng-messages-dynamic/content/js/controllers/CheckoutController.js
@@ -0,0 +1,24 @@
+function CheckoutController() {
+  this.details = {
+    username: '',
+    password: '',
+    coupon: 'summer-50'
+  };
+  this.onSubmit = function () {
+    console.log(this.details);
+  };
+  this.passwordErrors = [{
+    type: 'minlength',
+    text: 'Must be at least 8 characters!!'
+  },{
+    type: 'maxlength',
+    text: 'Must be 20 characters or less!!'
+  },{
+    type: 'pattern',
+    text: 'Password should contain at least one digit, at least one lower case, at least one upper case!!'
+  }];
+}
+
+angular
+  .module('app')
+  .controller('CheckoutController', CheckoutController);
